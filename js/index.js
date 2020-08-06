afb4044102fba52f9e9beecb2741bb2203e44e52
Benjamin PELAUDEIX <34833521+Benjamin-Pelaudeix@users.noreply.github.com>
2020-08-06 18:48:43 +0200

    Timer added for the game. Timer start when press 'start', timer stop when press 'stop' and timer reset when press 'reset'

--- js/index.js
+++ js/index.js
@@ -2,7 +2,13 @@ const playButton = document.getElementById('playButton');
 const closeButton = document.getElementById('close');
 const startButton = document.getElementById('start');
 const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
 const game = document.getElementById('game');
+const timerMilliseconds = document.getElementById('milliseconds');
+const timerHundreds = document.getElementById('hundreds');
+
+timerMilliseconds.innerHTML = "00";
+timerHundreds.innerHTML = "00";
 
 playButton.addEventListener('click', function() {
     playButton.style.display = 'none';
@@ -20,8 +26,6 @@ const countdown = document.getElementById('countdown');
 
 let randomTiming = Math.floor(Math.random() * 4001);
 
-let interval = setInterval(updateCountdown, 1000000000000);
-
 function updateCountdown() {
     let seconds = time;
     while (time !== 0) {
@@ -38,14 +42,56 @@ function updateCountdown() {
 
 function resetCountdown() {
     time = startingSeconds;
-    clearInterval(interval);
+    clearTimeout(interval);
     countdown.textContent = ' ';
 }
 
+let hundreds = 0, milliseconds = 0;
+
+function timer() {
+    hundreds++;
+    if (hundreds === 100) {
+        hundreds = 1;
+        milliseconds++;
+    }
+    if (milliseconds < 10) {
+        timerMilliseconds.innerHTML = "0" + milliseconds;
+    }
+    else {
+        timerMilliseconds.innerHTML = milliseconds;
+    }
+    if (hundreds < 10) {
+        timerHundreds.innerHTML = "0" + hundreds;
+    }
+    else {
+        timerHundreds.innerHTML = hundreds;
+    }
+}
+
+function timerStop() {
+    clearInterval(timerInterval);
+}
+
+function timerReset() {
+    clearInterval(timerInterval);
+    milliseconds = 0;
+    hundreds = 0;
+    timerMilliseconds.innerHTML = "0" + milliseconds;
+    timerHundreds.innerHTML = "0" + hundreds;
+}
+
 startButton.addEventListener('click', function() {
-    interval = setInterval(updateCountdown, randomTiming);
+    interval = setTimeout(updateCountdown, randomTiming);
+    if (updateCountdown() == 0) {
+        timerInterval = setInterval(timer, 10);
+    }
 });
 
 stopButton.addEventListener('click', function() {
     resetCountdown();
+    timerStop();
+});
+
+resetButton.addEventListener('click', function() {
+    timerReset();
 })
